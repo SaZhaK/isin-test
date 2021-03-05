d5d37fd06957b83457be5720ee62df209c600903
Alexander Kolomiets <kaa5843771@yandex.ru>
2021-03-05 22:21:32 +0300

    hello

--- src/main/java/Application/controllers/HelloWorld.java
+++ src/main/java/Application/controllers/HelloWorld.java
@@ -1,13 +1,20 @@
 package Application.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@Controller
 public class HelloWorld {
 
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/")
+    public String main() {
+        return "test";
     }
 }
---
+++ src/main/resources/static/functions.js
@@ -0,0 +1,6 @@
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "https://isintest.herokuapp.com/hello", false ); // false for synchronous request
+    xmlHttp.send( null );
+
+alert("lol");
